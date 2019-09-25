0ad1abb195e627f1e1c95c963739fd2dd93be443
Eli Kantor <eli.kantor@outlook.com>
2019-09-25 16:14:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -25,5 +25,16 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  h = nil
+  c = 0 
+  while array[c] do
+    if h == nil || array[c] < h
+      h = array[c]
+    end
+  c += 1 
+  end
+
+  return h 
+  
+  
 end
